ad2a25f8e3703981a824d3008f1af7c3379063b4
Jacob Chan <cks2306@gmail.com>
2026-07-30 01:45:52 +0100

    cse212_29July

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,11 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+        {
+            return 0;
+        }
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -40,6 +44,18 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            string Word2 = word + letters[i];
+            string Letters2 = letters.Remove(i, 1);
+            PermutationsChoose(results, Letters2, size, Word2);
+        }
     }
 
     /// <summary>
