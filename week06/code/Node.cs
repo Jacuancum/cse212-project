79cfd5f8618a45a9ba76d1f016d7e9d5197f9882
Jacob Chan <cks2306@gmail.com>
2026-08-08 21:33:56 +0100

    cse212_8Aug

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -17,29 +17,76 @@ public class Node
         {
             // Insert to the left
             if (Left is null)
+            {
                 Left = new Node(value);
+            }
             else
+            {
                 Left.Insert(value);
+            }
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
+            {
                 Right = new Node(value);
+            }
             else
+            {
                 Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            if (Left is null)
+            {    
+                return false;
+            }
+            else
+            {
+                return Left.Contains(value);
+            }
+        }
+        else
+        {
+            if (Right is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Right.Contains(value);
+            }
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left is not null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+
+        if (Right is not null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
